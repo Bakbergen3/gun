3d42d58ad503b0189f0db566b2c7c64d6d2e56ac
Bakbergen Ryskulov <bakbergen3@gmail.com>
2020-01-03 17:06:31 +0600

    Created a skeleton of the projects

---
+++ gun_ref.py
@@ -0,0 +1,29 @@
+from random import randrange as rnd
+from random import choice
+import tkinter as tk
+import math
+
+
+def main():
+    """
+    Executes the application
+    """
+    pass
+
+
+class Bullet:
+    """
+    Bullet, which hits the targets.
+    """
+
+
+class Gun:
+    pass
+
+
+class Target:
+    pass
+
+
+if __name__ == __name__:
+    pass
