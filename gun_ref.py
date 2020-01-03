32a5d6ce37a7b2ca298c4b5ffa2432001b68b539
Bakbergen Ryskulov <bakbergen3@gmail.com>
2020-01-03 19:52:41 +0600

    The bullet hits the target and text appears on screen

--- gun_ref.py
+++ gun_ref.py
@@ -4,7 +4,7 @@ import tkinter as tk
 import math
 import time
 
-#Global params
+# Global params
 WIDTH = 800
 HEIGHT = 600
 
@@ -17,14 +17,12 @@ def main():
     global bullet, target, text_screen, gun, number_of_shots, bullets
 
     root = tk.Tk()
-    frame = tk.Frame(root)
     root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
     canvas = tk.Canvas(root, bg='white')
     canvas.pack(fill=tk.BOTH, expand=1)
 
     target = Target()
-    text_screen = canvas.create_text(400, 300, text='To be filled',
-                                    font='Times 28')
+    text_screen = canvas.create_text(400, 20, text='', font='Times 20')
     gun = Gun()
     number_of_shots = 0
     bullets = []
@@ -45,8 +43,14 @@ def time_handler():
     while target.life or bullets:
         for bullet in bullets:
             bullet.move()
-            #Add target hit
-        
+            if bullet.hit_target(target) and target.life:
+                target.life = 0
+                target.hit()
+                canvas.bind('<Button-1>', '')
+                canvas.bind('<ButtonRelease-1>', '')
+                canvas.itemconfig(text_screen, text='Вы уничтожили цель за ' +
+                                                    str(number_of_shots) +
+                                                    ' выстрелов')
         canvas.update()
         time.sleep(0.03)
         gun.targetting()
@@ -97,7 +101,13 @@ class Bullet:
         self.x += self.vx
         self.y -= self.vy
         self.set_coords()
-    #FIXME: Add collision motion with other bullets 
+
+    def hit_target(self, object):
+        distance = math.sqrt((self.x - object.x)**2 + (self.y - object.y)**2)
+        if distance <= self.r + object.r:
+            return True
+        else:
+            return False
 
 
 class Gun:
@@ -125,8 +135,8 @@ class Gun:
         new_bullet = Bullet(20 + max(self.power, 20) * math.cos(self.angle),
                             450 + max(self.power, 20) * math.sin(self.angle))
         new_bullet.r += 5
-        self.angle = math.atan((event.y - new_bullet.y)
-                                / (event.x - new_bullet.x))
+        self.angle = math.atan((event.y - new_bullet.y) /
+                               (event.x - new_bullet.x))
         new_bullet.vx = self.power * math.cos(self.angle)
         new_bullet.vy = - self.power * math.sin(self.angle)
         bullets += [new_bullet]
@@ -141,8 +151,8 @@ class Gun:
             self.angle = math.atan((event.y - 450) / (event.x - 20))
         if self.gun_on:
             canvas.coords(self.gun_id, 20, 450,
-                        20 + max(self.power, 20) * math.cos(self.angle),
-                        450 + max(self.power, 20) * math.sin(self.angle))
+                          20 + max(self.power, 20) * math.cos(self.angle),
+                          450 + max(self.power, 20) * math.sin(self.angle))
 
     def power_up(self):
         """
@@ -168,15 +178,17 @@ class Target:
         self.y = rnd(300, HEIGHT - self.r)
         self.color = choice(['blue', 'green', 'red', 'brown'])
         self.target_id = canvas.create_oval(self.x - self.r, self.y - self.r,
-                                        self.x + self.r, self.y + self.r,
-                                        fill=self.color, width=0)
+                                            self.x + self.r, self.y + self.r,
+                                            fill=self.color, width=0)
 
     def hit(self, points=1):
         """
         When the bullet hits the target.
         """
         canvas.coords(self.target_id, -10, -10, -10, -10)
-        #FIX: Add points counter
+        self.points += 1
+        canvas.itemconfig(text_screen, text='Score: ' + str(self.points))
+
 
 if __name__ == __name__:
     main()
